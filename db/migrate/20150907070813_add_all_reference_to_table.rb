15c9693e6a573810814cccdc6146f3dd6eab17f9
kjj6198 <kjj6198@gmail.com>
2015-09-07 16:01:27 +0800

    add reference to tables

--- db/migrate/20150907070813_add_all_reference_to_table.rb
+++
@@ -1,7 +0,0 @@
-class AddAllReferenceToTable < ActiveRecord::Migration
-  def change
-    add_reference :bugs, :user, index: true
-    add_reference :comments, :bug, index: true
-    add_reference :comments, :user, index: true
-  end
-end
