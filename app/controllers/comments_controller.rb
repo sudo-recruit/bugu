6847d09698ab260636f796e25b86c90d35516828
kjj6198 <kjj6198@gmail.com>
2015-09-08 10:09:10 +0800

    set markdown editor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   	@bug = Bug.find(params[:bug_id])
   	@comment = @bug.comments.create(comment_params)
   	@comment.bug.status = @comment[:change_status]
-  	@statuses = Comment.change_statuses
+  	@status = Comment.change_statuses
 
 
   	if @comment.save && @comment.bug.save
